1eff8588dec6437f8b75be70c840834df356ffb8
Oleksandr <dio@ukr.net>
2024-08-14 19:41:27 +0300

    refactored exercise 04

--- 04/lifecycle.py
+++ 04/lifecycle.py
@@ -5,110 +5,170 @@ THINKING_TIME = 2, 5
 EATING_TIME = 2, 5
 PHILOSOPHER_COUNT = 5
 FORK_COUNT = PHILOSOPHER_COUNT
-DAILY_CYCLE = 10
+DAILY_CYCLE = 3
+MONITORING_INTERVAL = 1
 
 
-class Philosophers:
-    async def acquire_fork(self, fork, philosopher: int):
-        """Get exclusive access to the fork"""
+class State:
+    INACTIVE = 'inactive'
+    THINKING = 'thinking'
+    WAITING_FORKS = 'waiting'
+    EATING = 'eating'
+
+
+class Fork:
+    def __init__(self, number: int):
+        self.owner = 0
+        self.number = number
+        self._initialize()
+
+    def _initialize(self):
+        """Low-level method, must be overridden: initialize internal async object"""
         pass
 
-    async def acquire_forks(self, forks: tuple, philosopher: int):
+    async def acquire(self, owner_id: int):
+        await self._acquire()
+        self.owner = owner_id
+        print('Member', owner_id, 'acquired the fork', self.number)
+
+    async def release(self):
+        await self._release()
+        owner_id, self.owner = self.owner, 0
+        print('Member', owner_id, 'released the fork', self.number)
+
+    async def _acquire(self):
+        """Low-level method, must be overridden """
+        pass
+
+    async def _release(self):
+        """Low-level method, must be overridden """
+        pass
+
+
+class QueueFork(Fork):
+    def _initialize(self):
+        self.queue = asyncio.Queue(1)
+
+    async def _acquire(self):
+        await self.queue.join()
+        await self.queue.put(self.owner)
+
+    async def _release(self):
+        await self.queue.get()
+        self.queue.task_done()
+
+
+class LockFork(Fork):
+    def _initialize(self):
+        self.lock = asyncio.Lock()
+
+    async def _acquire(self):
+        await self.lock.acquire()
+
+    async def _release(self):
+        self.lock.release()
+
+
+class EventFork(Fork):
+    def _initialize(self):
+        self.event = asyncio.Event()
+        self.event.set()  # Initial state of a fork is "free"
+
+    async def _acquire(self):
+        await self.event.wait()
+        self.event.clear()
+
+    async def _release(self):
+        self.event.set()
+
+
+class Philosopher:
+
+    def __init__(self, number: int, forks: tuple):
+        self.state = State.INACTIVE
+        self.number = number
+        self.forks = forks
+        self.meal_count = 0
+
+    async def acquire_forks(self):
         """Get exclusive access to the both forks"""
-        await asyncio.gather(*[self.acquire_fork(fork, philosopher) for fork in forks])
-        print(f'Philisopher {philosopher} has acquired both forks')
 
-    def create_fork(self):
-        """Create a "fork" synchronization object """
-        pass
+        self.set_state(State.WAITING_FORKS)
+        await asyncio.wait([
+            asyncio.create_task(fork.acquire(self.number)) for fork in self.forks
+        ], return_when=asyncio.ALL_COMPLETED)
 
-    @staticmethod
-    async def eating(philosopher: int):
-        duration = random.randint(*EATING_TIME)
-        print(f'Philisopher {philosopher} is eating')
-        await asyncio.sleep(duration)
+    def set_state(self, new_state):
+        old_state, self.state = self.state, new_state
+        print("Member", self.number, f"changed state from '{old_state}' to '{self.state}'")
 
-    @staticmethod
-    async def thinking(philosopher: int):
-        duration = random.randint(*THINKING_TIME)
-        print(f'Philisopher {philosopher} is thinking')
-        await asyncio.sleep(duration)
-
-    async def release_fork(self, fork, philosopher: int):
-        """Release exclusive access to the fork"""
-        pass
-
-    async def release_forks(self, forks: tuple, philosopher: int):
+    async def release_forks(self):
         """Release exclusive access to both forks"""
-        for fork in forks:
-            await self.release_fork(fork, philosopher)
-        print(f'Philisopher {philosopher} has released both forks')
 
-    async def philosopher(self, number: int, forks):
+        for fork in self.forks:
+            await fork.release()
+
+    async def eating(self):
+        self.set_state(State.EATING)
+        duration = random.randint(*EATING_TIME)
+        await asyncio.sleep(duration)
+        self.meal_count += 1
+
+    async def thinking(self):
+        self.set_state(State.THINKING)
+        duration = random.randint(*THINKING_TIME)
+        await asyncio.sleep(duration)
+
+    async def daily_life(self):
         """Task describes "daily life" of each philosopher """
-        meals = 0
+
         try:
             for _ in range(DAILY_CYCLE):
-                await self.thinking(number)
-                await self.acquire_forks(forks, number)
-                await self.eating(number)
-                meals += 1
-                await self.release_forks(forks, number)
+                await self.thinking()
+                await self.acquire_forks()
+                await self.eating()
+                await self.release_forks()
         finally:
-            print(f'Pholosopher #{number} ate {meals} times')
+            self.set_state(State.INACTIVE)
+        return self.meal_count
+
+
+class PhilosopherClub:
+
+    def __init__(self, fork_class):
+        self.forks = [fork_class(i+1) for i in range(FORK_COUNT)]
+        fork_pairs = [
+            (self.forks[i], self.forks[(i + 1) % FORK_COUNT]) for i in range(FORK_COUNT)
+        ]
+        self.members = [
+            Philosopher(i+1, fork_pairs[i]) for i in range(PHILOSOPHER_COUNT)
+        ]
+
+    async def monitoring(self):
+        done = 0
+        while done < len(self.members):
+            await asyncio.sleep(MONITORING_INTERVAL)
+            states = [m.state for m in self.members]
+            done = states.count(State.INACTIVE)
+            print('States are: ', ', '.join(states))
+            print('Forks owners: ', ', '.join([str(f.owner) for f in self.forks]))
+        for m in self.members:
+            print(f'Member {m.number} ate {m.meal_count} times')
+
 
     async def daily_life(self):
         """Task describes "daily life" of all philosophers together"""
-        forks = [self.create_fork() for _ in range(FORK_COUNT)]
-        tasks = []
-        for i, fork in enumerate(forks, 1):
-            fork_pair = forks[i-1], forks[i % FORK_COUNT]
-            tasks.append(
-                asyncio.create_task(self.philosopher(i, fork_pair))
-            )
-        await asyncio.gather(*tasks)
 
-
-class PhilosophersEvent(Philosophers):
-    async def acquire_fork(self, fork, philosopher: int):
-        await fork.wait()
-        fork.clear()
-
-    def create_fork(self):
-        fork = asyncio.Event()
-        fork.set()  # Initial state of a fork is "free"
-        return fork
-
-    async def release_fork(self, fork, philosopher: int):
-        fork.set()
-
-
-class PhilosophersLock(Philosophers):
-    async def acquire_fork(self, fork, philosopher: int):
-        await fork.acquire()
-
-    def create_fork(self):
-        return asyncio.Lock()
-
-    async def release_fork(self, fork, philosopher: int):
-        fork.release()
-
-
-class PhilosophersQueue(Philosophers):
-    async def acquire_fork(self, fork, philosopher: int):
-        await fork.join()
-        await fork.put(philosopher)
-
-    def create_fork(self):
-        return asyncio.Queue(1)
-
-    async def release_fork(self, fork, philosopher: int):
-        await fork.get()
-        fork.task_done()
+        tasks = [
+            asyncio.create_task(p.daily_life(), name=str(p.number)) for p in self.members
+        ]
+        monitor_task = asyncio.create_task(self.monitoring())
+        await asyncio.gather(monitor_task, *tasks)
 
 
 if __name__ == '__main__':
-    methods = [PhilosophersLock, PhilosophersEvent, PhilosophersQueue]
+    methods = [LockFork, EventFork, QueueFork]
     method = random.choice(methods)
     print('Using', method.__name__)
-    asyncio.run(method().daily_life())
+    club = PhilosopherClub(method)
+    asyncio.run(club.daily_life())
